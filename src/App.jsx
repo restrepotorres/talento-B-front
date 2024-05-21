9e730fe9928e198181369e19bdb5390997bafdd4
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2024-05-20 22:06:09 -0500

    feat: add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,41 @@ import Login from "./Login";
 import { Route, Routes } from "react-router-dom";
 import Search from "./Search";
 import EditDialog from "./EditDialog"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: "#181A1B",
+      paper: "#0C1219",
+      greeni:"#FFFFFF"
+    },
 
+    primary: {main:"#488bbf"},
+    secondary: { main:"#FFFFFF"},
+    error: { main:"#b71f1f"},
+    green: { main:"#29aa29"}
+  },
+});
 
 function App() {
 
 
-    return (<>
+  return (<>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Routes>
         <Route path="/search" element={<Search />} />
-        <Route path="/edit" element={<EditDialog />} />
+        <Route path="/edit/:scriptid" element={<EditDialog />} />
         <Route path="*" element={<Search />} />
         <Route path="/" element={<Login />} />
       </Routes>
 
-      {/* <Login></Login> */}</>
-    );
-  }
+      {/* <Login></Login> */} </ThemeProvider></>
+  );
+}
 
-  export default App;
+export default App;
